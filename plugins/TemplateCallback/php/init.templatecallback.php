c4c501a6439139cfd72f153829b044d5fc0c6fa2
Shmuel Fomberg <shmuelfomberg@gmail.com>
2012-06-15 11:14:15 +0900

    completed widgetset overriding

--- plugins/TemplateCallback/php/init.templatecallback.php
+++ plugins/TemplateCallback/php/init.templatecallback.php
@@ -1,8 +1,8 @@
 <?php
 
 function __handle_widgetset($args, &$ctx) {
-    require_once('callback_lib.php');
-    require_once('function.mttemplatecallback.php');
+    // require_once('callback_lib.php');
+    // require_once('function.mttemplatecallback.php');
     $blog_id = $args['blog_id'];
     $blog_id or $blog_id = $ctx->stash('blog_id');
     $blog_id or $blog_id = 0;
@@ -11,28 +11,30 @@ function __handle_widgetset($args, &$ctx) {
     if (!$widgetmanager || !$cb_name) 
         return;
     $tmpl = $ctx->mt->db()->get_template_text($ctx, $widgetmanager, $blog_id, 'widgetset', $args['global']);
-    if ( !isset($tmpl) || !$tmpl ) {
-        return '';
-    }
-    $inclues = array();
-    preg_match_all('/<mt:include widget="[^"]">/', $tmpl, $matches);
-
-    if ($matches && count($matches)) {
-        $step = 4.0 / count($matches);
-        $priority = 3.0;
-        list($i_cb_name) = explode(' ', $cb_name);
-        global $_callback_registry;
-        if (!array_key_exists($i_cb_name, $_callback_registry)) {
-            $_callback_registry[$i_cb_name] = array();
+    if ( isset($tmpl) && $tmpl ) {
+        preg_match_all('/<mt:include widget="[^"]*">/', $tmpl, $matches);
+        if ($matches && count($matches)) {
+            $matches = $matches[0];
         }
-        foreach ($matches as $m) {
-            $rec = array(
-                'priority' => $priority,
-                'template' => $m,
-                'name'     => $i_cb_name,
-            );
-            $_callback_registry[$i_cb_name][] = $rec;
-            $priority += $step;
+
+        if ($matches && count($matches)) {
+            $step = 4.0 / count($matches);
+            $priority = 3.0;
+            list($i_cb_name) = explode(' ', $cb_name);
+            $i_cb_name = 'publish.' . $i_cb_name;
+            global $_callback_registry;
+            if (!array_key_exists($i_cb_name, $_callback_registry)) {
+                $_callback_registry[$i_cb_name] = array();
+            }
+            foreach ($matches as $m) {
+                $rec = array(
+                    'priority' => $priority,
+                    'template' => $m,
+                    'name'     => $i_cb_name,
+                );
+                $_callback_registry[$i_cb_name][] = $rec;
+                $priority += $step;
+            }
         }
     }
     return smarty_function_mttemplatecallback( array('name' => $cb_name), $ctx );
